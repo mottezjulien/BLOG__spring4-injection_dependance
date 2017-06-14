540f507140bd6b4489b4c3ad40ffcadd4b554e00
Julien <julien.mottez@direct-fleet.com>
2017-06-14 16:51:14 +0200

    [Spring4-Maven-Helloworld] Simplify the project : - Only a simple injection. - Delete scope presentation.

--- src/main/java/fr/jmottez/lessons/spring/spring4/RunnerApplication.java
+++ src/main/java/fr/jmottez/lessons/spring/spring4/RunnerApplication.java
@@ -1,7 +1,6 @@
 package fr.jmottez.lessons.spring.spring4;
 
 import fr.jmottez.lessons.spring.spring4.service.AService;
-import fr.jmottez.lessons.spring.spring4.service.APropotypeService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -14,20 +13,6 @@ public class RunnerApplication {
 		AService aService = (AService) context.getBean("AService");
 		aService.callBService();
 
-		aService.setValue("any Value AService");
-		System.out.println("AService:" + aService.getValue());
-
-		aService = (AService) context.getBean("AService");
-		System.out.println("AService:" + aService.getValue());
-
-		APropotypeService aPropotypeService = (APropotypeService) context.getBean("a_prototype_service");
-		aPropotypeService.callBService();
-
-		aPropotypeService.setValue("any Value APropotypeService");
-		System.out.println("ASingletonService:" + aPropotypeService.getValue());
-		aPropotypeService = (APropotypeService) context.getBean("a_prototype_service");
-		System.out.println("ASingletonService:" + aPropotypeService.getValue());
-
-	} 
+	}
 
 }
--- src/main/java/fr/jmottez/lessons/spring/spring4/service/AService.java
+++ src/main/java/fr/jmottez/lessons/spring/spring4/service/AService.java
@@ -4,21 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-//@Scope(value = "singleton")
 public class AService {
 
 	@Autowired
 	private BService bService;
-
-	private String value = "";
-
-	public String getValue() {
-		return value;
-	}
-
-	public void setValue(String value) {
-		this.value = value;
-	}
 
 	public void callBService() {
 		bService.call();
